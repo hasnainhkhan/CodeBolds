819c65eaa12d3b0b7ed07626aebfc28fe523ca7a
hasnainhkhan <hhk2170@gmail.com>
2025-03-25 22:56:02 +0530

    project added

---
+++ CodeBolds/src/main/java/com/CodeBolds/info/CodeBoldsApplication.java
@@ -0,0 +1,13 @@
+package com.CodeBolds.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBoldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBoldsApplication.class, args);
+	}
+
+}
---
+++ CodeBolds/src/test/java/com/CodeBolds/info/CodeBoldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeBolds.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBoldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
