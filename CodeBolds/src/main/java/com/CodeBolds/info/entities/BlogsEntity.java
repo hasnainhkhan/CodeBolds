9ec411010806a06e70ed38695a1b70a8e3cb7508
hasnainhkhan <hhk2170@gmail.com>
2025-03-26 00:49:51 +0530

    structure created

---
+++ CodeBolds/src/main/java/com/CodeBolds/info/entities/BlogsEntity.java
@@ -0,0 +1,5 @@
+package com.CodeBolds.info.entities;
+
+public class BlogsEntity {
+
+}
--- CodeBolds/src/main/java/com/CodeBolds/info/service/BlogService.java
+++ CodeBolds/src/main/java/com/CodeBolds/info/service/BlogService.java
@@ -1,5 +1,8 @@
 package com.CodeBolds.info.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BlogService {
 
 }
